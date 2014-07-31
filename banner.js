8d8f7f96a828689c8d6ab8b6b9df7674c8564c8b
April Luo <april.luo@fivestars.com>
2014-07-30 20:31:14 -0700

    fugly banner

--- banner.js
+++ banner.js
@@ -1,2 +1,12 @@
 alert("hello");
-document.body.style.backgroundColor="blue";
+var banner_raw_css = '<style>#FS-banner{background-color:#0076B5;text-align:center;}</style>';
+var banner_raw_html = '<div id="FS-banner"><h3 style="color:white;">This business is a FiveStars location!</h3></div>';
+
+$("head").append(banner_raw_css);
+$(banner_raw_html).insertAfter(".app-header");
+
+/*
+$(document).ready(function(){
+   $("#banner").load("banner.html");
+});
+*/
